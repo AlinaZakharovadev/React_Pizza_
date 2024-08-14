8f76698cb6accac0794cdf78967963c5d851db1b
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-14 18:30:10 +0200

    made updates

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import "./scss/app.scss";
 import Header from "./components-pizza/header/Header";
 import Categories from "./components-pizza/categories/Categories";
 import Sort from "./components-pizza/sort/Sort";
-import Pizza from "./components-pizza/pizza/Pizza";
+import PizzaBlock from "./components-pizza/pizza/PizzaBlock";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={450} />
+            <PizzaBlock title="Сербская" price={700} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
           </div>
         </div>
       </div>
--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import Pizza from "../../components-pizza/pizza/Pizza";
+import PizzaBlock from "../../components-pizza/pizza/PizzaBlock";
 
 function homePage() {
   return (
     <div>
-      <h2 class="content__title">Все пиццы</h2>
-      <div class="content__items">
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
       </div>
     </div>
   );
