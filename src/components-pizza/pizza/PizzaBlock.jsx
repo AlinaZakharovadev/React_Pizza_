e447097ac79f1b7126cd3787d1eddfc693d8cf88
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-14 18:52:39 +0200

    applied useState Hook for pizza add button

--- src/components-pizza/pizza/PizzaBlock.jsx
+++ src/components-pizza/pizza/PizzaBlock.jsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 function PizzaBlock({ title, price }) {
   const [pizzaCount, setPizzaCount] = useState(0);
 
+  const onClickAddButton = () => {
+    setPizzaCount(pizzaCount + 1);
+  };
+
   return (
     <div className="pizza-block">
       <img
@@ -24,7 +28,10 @@ function PizzaBlock({ title, price }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button className="button button--outline button--add">
+        <button
+          onClick={onClickAddButton}
+          className="button button--outline button--add"
+        >
           <svg
             width="12"
             height="12"
