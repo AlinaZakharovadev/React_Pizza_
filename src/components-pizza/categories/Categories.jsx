2a3f29cb4aced87c8b6eeeb41b9258d94669a160
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-14 19:47:19 +0200

    render list of categories

--- src/components-pizza/categories/Categories.jsx
+++ src/components-pizza/categories/Categories.jsx
@@ -6,7 +6,7 @@ function Categories() {
   const categories = [
     "Все",
     "Мясные",
-    "Вегетарианская",
+    "Вегетарианские",
     "Гриль",
     "Острые",
     "Закрытые",
@@ -18,42 +18,14 @@ function Categories() {
   return (
     <div className="categories">
       <ul>
-        <li
-          onClick={() => onClickCategory(0)}
-          className={activeCategory === 0 ? "active" : ""}
-        >
-          Все
-        </li>
-        <li
-          onClick={() => onClickCategory(1)}
-          className={activeCategory === 1 ? "active" : ""}
-        >
-          Мясные
-        </li>
-        <li
-          onClick={() => onClickCategory(2)}
-          className={activeCategory === 2 ? "active" : ""}
-        >
-          Вегетарианская
-        </li>
-        <li
-          onClick={() => onClickCategory(3)}
-          className={activeCategory === 3 ? "active" : ""}
-        >
-          Гриль
-        </li>
-        <li
-          onClick={() => onClickCategory(4)}
-          className={activeCategory === 4 ? "active" : ""}
-        >
-          Острые
-        </li>
-        <li
-          onClick={() => onClickCategory(5)}
-          className={activeCategory === 5 ? "active" : ""}
-        >
-          Закрытые
-        </li>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickCategory(i)}
+            className={activeCategory === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
