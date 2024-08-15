05a0ef5e663d6100c1b464b37c0573831242c368
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-15 13:36:24 +0200

    Used Spread Syntax

--- src/App.js
+++ src/App.js
@@ -20,10 +20,12 @@ function App() {
           <div className="content__items">
             {pizzas.map((obj) => (
               <PizzaBlock
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
+                {...obj}
+                // title={obj.title}
+                // price={obj.price}
+                // imageUrl={obj.imageUrl}
+                // sizes={obj.sizes}
+                // types={obj.types}
               />
             ))}
           </div>
