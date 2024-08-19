286a831cd773ec4dbb846e200a20cde91d441da2
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-19 19:27:39 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./scss/app.scss";
-import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PizzaHomePage from "./routes/homePage/PizzaHomePage";
 import NotFound from "./routes/notfound/NotFound";
