29367a71603ce8a491b9bc55d92594f472e623ff
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-21 17:16:04 +0200

    Updated scss

--- src/components-pizza/cartEmpty/CartEmpty.jsx
+++ src/components-pizza/cartEmpty/CartEmpty.jsx
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 
 function CartEmpty() {
   return (
-    <div class="content">
-      <div class="container">
+    <div className="content">
+      <div className="container">
         <div className="cart--empty">
-          <h2>
-            Корзина пустая <icon>😕</icon>
-          </h2>
+          <h2>Корзина пустая 😕</h2>
           <p>Для того, чтобы заказать пиццу, перейди на главную страницу.</p>
           <img src="/img/empty-cart.png" alt="Empty cart" />
           <Link to="/" className="button button--black">
